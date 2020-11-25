9b2079260af4e93fbc8b73501bed8aabce578abe
juanmiloz <juanmiloz@hotmail.com>
2020-11-25 07:51:05 -0500

    Added new class Main and controller

---
+++ src/GUI/Main.java
@@ -0,0 +1,25 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("eclipse.fxml"));
+		
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Eclipse :: The dance of the sun and the moon");
+		primaryStage.show();
+	}
+
+}
---
+++ src/GUI/eclipseGUI.java
@@ -0,0 +1,40 @@
+package GUI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+
+public class eclipseGUI {
+
+	@FXML
+	private Circle sun;
+
+	@FXML
+	private Circle moon;
+
+	@FXML
+	private Circle crater1;
+
+	@FXML
+	private Circle crater2;
+
+	@FXML
+	private Circle crater3;
+
+	
+	public eclipseGUI() {
+		// TODO Auto-generated constructor stub
+	}
+	
+    @FXML
+    void left(ActionEvent event) {
+
+    }
+
+    @FXML
+    void right(ActionEvent event) {
+
+    }
+
+
+}
